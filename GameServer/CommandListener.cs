279d2ff17ed81f65a5dbcd8f1f6088dc006c2a91
BuildTools <unconfigured@null.spigotmc.org>
2017-07-02 22:28:37 +0200

    refactor

--- GameServer/CommandListener.cs
+++ GameServer/CommandListener.cs
@@ -26,10 +26,12 @@ namespace GameServer
                 Console.WriteLine("Name can not be empty");
                 return;
             }
-            Globals.ConfigurationFile.SetValue("name", name);
-            Globals.ConfigurationFile.Apply();
-            Globals.Name = name;
-            Console.WriteLine("Servername changed to: " + name);
+			Globals.ConfigurationFile.SetValue("name", name);
+			Globals.ConfigurationFile.Apply();
+			Globals.Name = name;
+			Console.WriteLine("Servername changed to: " + name);
+
+
         }, "Changes the name of this server. Type 'name ?' to get more information");
         #endregion
         #region "ChangePort"
@@ -54,14 +56,18 @@ namespace GameServer
         #region "Help"
         public static Cmd Help = new Cmd(new string[] { "?", "help" }, (string[] args) =>
         {
-            Console.WriteLine("\n\nFollowing commands are registered");
-            Console.WriteLine("Type 'help <command>' to show information about the command");
+
 
             string specificCmd = "";
 
             if (args.Length == 1)
             {
                 specificCmd = args[0];
+            }
+            else
+            {
+                Console.WriteLine("\n\nFollowing commands are registered");
+                Console.WriteLine("Type 'help <command>' to show information about the command");
             }
 
             foreach (Cmd cmd in CommandListener.Commands)
@@ -76,10 +82,18 @@ namespace GameServer
             }
         }, "show list of commands");
         #endregion
-
+        #region "Online"
+        public static Cmd Online = new Cmd(new string[] { "online" }, (string[] args) =>
+        {
+            Console.WriteLine(MainClass._accounts.Count + " Clients are connected to the server");
+            Console.WriteLine(MainClass._accounts.Count + " Accounts are logged in");
+            Console.WriteLine(MainClass._accounts.Count + " Accounts are ingame");
+        }, "Show online players");
+        #endregion
     }
 
     public delegate void CmdAction(string[] args);
+
     public class Cmd
     {
         public List<string> Keywords { get; private set; }
@@ -100,21 +114,11 @@ namespace GameServer
     public static class CommandListener
     {
         private static readonly List<Cmd> _commands = new List<Cmd>();
-
         public static List<Cmd> Commands { get { return _commands; } }
-
-        static CommandListener()
-        {
-        }
 
         public static void AddCommand(Cmd cmd)
         {
             _commands.Add(cmd);
-        }
-
-        public static void AddCommand(string[] keywords, CmdAction action)
-        {
-            _commands.Add(new Cmd(keywords, action));
         }
 
         public static void HandleCommand(string input)
