b4efcb423d94d040ce3e0984c9d1c9fce5184a3d
BuildTools <unconfigured@null.spigotmc.org>
2017-07-02 14:48:02 +0200

    refactor + added Configuration

--- GameServer/Server.cs
+++ GameServer/Server.cs
@@ -3,32 +3,33 @@ using Network;
 using Network.Packages;
 using System.Collections.Generic;
 using System.Net;
+using FileManager;
 
 namespace GameServer
 {
+    static class Globals{
+        public static string Name = "Example Name";
+        public static int Port = 5090;
+        public static List<long> Admins = new List<long>();
+    }
+
     class MainClass
     {
-
         public static Dictionary<IPEndPoint, Account> _accounts = new Dictionary<IPEndPoint, Account>();
         public static UdpServer _server;
 
         public static void Main(string[] args)
         {
             Console.WriteLine("Starting Server ...");
-            int port = 5090;
-            for (int i = 0; i < args.Length; i++)
-            {
-                if (args[i] == "-p")
-                {
-                    port = int.Parse(args[i + 1]);
-                }
-            }
-            Console.WriteLine("Listening on Port: " + port);
-            _server = new UdpServer(port);
+            LoadConfigurationFile();
+            ApplyStartupArgs(args);
+
+            Console.WriteLine("Listening on Port: " + Globals.Port);
+            _server = new UdpServer(Globals.Port);
             _server.DataReceived += OnServerReceive;
             _server.Start();
             Console.WriteLine("Server started");
-
+            Console.WriteLine("Server Name: " + Globals.Name);
             while (true)
             {
                 string input = Console.ReadLine().ToLower();
@@ -39,6 +40,42 @@ namespace GameServer
                 }
             }
             Environment.Exit(0);
+        }
+
+        public static void LoadConfigurationFile(){
+			Console.WriteLine("Loading Configuration");
+			Config config = new Config("config.ini");
+			foreach (Config.KeyValue keyvalue in config.GetConfigs())
+			{
+                switch (keyvalue.Key.ToLower())
+				{
+                    case "name":
+                        Globals.Name = keyvalue.Value;
+                        break;
+					case "port":
+                        int.TryParse(keyvalue.Value, out Globals.Port);
+						break;
+                    case "admins":
+                        foreach(string id in keyvalue.Value.Split(',')){
+                            if (long.TryParse(id, out long Id))
+                            {
+                                Globals.Admins.Add(Id);
+                            }
+                        }
+                        break;
+				}
+			}
+            Console.WriteLine("Loading Configuration done.");
+        }
+
+        public static void ApplyStartupArgs(string[] args){
+			for (int i = 0; i < args.Length; i++)
+			{
+				if (args[i] == "-p")
+				{
+					int.TryParse(args[i + 1], out Globals.Port);
+				}
+			}
         }
 
         public static void OnServerReceive(object sender, NetworkReceiveEventArgs e)
