d44c70b76980090668df9ba224b5efc9388a401f
BuildTools <hellfire1470@users.noreply.github.com>
2017-07-13 16:13:45 +0200

    moved to Database Project

---
+++ Database/Sql.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using Mono.Data.Sqlite;
+using System.Collections.Generic;
+
+namespace Database
+{
+	public class SQL
+	{
+		private readonly string _path;
+		private readonly SqliteConnection _connection;
+
+		public delegate void OnCreatedHandler(SQL sql);
+
+		public SQL(string path, OnCreatedHandler OnCreated = null)
+		{
+			_path = path;
+			bool raiseOnCreatedEvent = false;
+			if (!File.Exists(path))
+			{
+				SqliteConnection.CreateFile(path);
+				raiseOnCreatedEvent = true;
+			}
+
+			_connection = new SqliteConnection("Data Source=" + path + ";Version=3;");
+			_connection.Open();
+
+			if (raiseOnCreatedEvent && OnCreated != null)
+				OnCreated(this);
+		}
+
+		public int ExecuteNonQuery(string sql)
+		{
+			SqliteCommand cmd = new SqliteCommand(sql, _connection);
+			return cmd.ExecuteNonQuery();
+		}
+
+		public Dictionary<int, Dictionary<string, string>> ExecuteQuery(string sql)
+		{
+			Dictionary<int, Dictionary<string, string>> ret = new Dictionary<int, Dictionary<string, string>>();
+
+			SqliteCommand cmd = new SqliteCommand(sql, _connection);
+			SqliteDataReader dr = cmd.ExecuteReader();
+
+			int row = 0;
+
+			while (dr.Read())
+			{
+
+				int field_count = dr.FieldCount;
+				Dictionary<string, string> data = new Dictionary<string, string>();
+				for (int field = 0; field < field_count; field++)
+				{
+					data[dr.GetName(field)] = dr.GetValue(field).ToString();
+				}
+				ret[row++] = data;
+			}
+			return ret;
+		}
+	}
+}
