d2b9d23dccf32807c90a2cf92c23817cc8c32d6d
BuildTools <fischer.alexander93@googlemail.com>
2017-08-14 12:20:33 +0200

    update
    + EntityQuality
    + ItemQuality
    + ItemType
    + StatType
    + FractionType
    + Map
    + CharacterAnimationType
    Changed Class to ClassType
    Change Race to RaceType

--- GameData/Base.cs
+++ GameData/Base.cs
@@ -1,27 +1,80 @@
 ﻿namespace GameData
 {
-
-    public enum RessourceType
+    public enum ErrorResult
     {
-        None, Mana, Energy, Rage
+        UnknownError, Success, WrongData, InvalidGameKey, Banned, CharacterLimit, InvalidName, NameExists
+    }
+ 
+	public enum EntityQuality
+	{
+		// Important: do not change sorting order
+		Normal, Rare, Elite, Boss, Legendary
+	}
+	public enum ItemQuality
+	{
+		// Important: do not change sorting order
+		Useless, Normal, Magic, Rare, Epic, Legendary
+	}
+	public enum ItemType
+	{
+		// Important: do not change sorting order
+		Helmet, Shoulder, Chest
+	}
+
+	public enum RessourceType
+	{
+		// Important: do not change sorting order
+		None, Mana, Energy, Rage
+	}
+
+    public enum StatType
+    {
+        Strength, Vitality, Intelligence
+    }
+
+    public enum FractionType
+    {
+        // Important: do not change sorting order
+        Neutral, A, B, C
+    }
+
+    public class Map
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 
     public class Location
     {
-        public int Map;
-        public string Name;
-        public float CoordX;
-        public float CoordY;
-        public float CoordZ;
+        public Location(){}
+        public Map Map { get; set; }
+        public float CoordX { get; set; }
+        public float CoordY { get; set; }
+        public float CoordZ { get; set; }
+
+        public Location(Map map, float x, float y, float z)
+        {
+            Map = map;
+            CoordX = x;
+            CoordY = y;
+            CoordZ = z;
+        }
     }
 
-    public enum Class
+    public enum CharacterAnimationType
     {
-        Mage, Warrior, Priest, Rouge
+        Stop, Stand, Move, Run, Jump, Fall, Sit
     }
 
-    public enum Race
+    public enum ClassType
     {
-        Troll, Orc, Human, Goblin
+        // Important: do not change sorting order
+		Mage, Warrior, Priest, Rouge
+    }
+
+    public enum RaceType
+    {
+		// Important: do not change sorting order
+		Troll, Orc, Human, Goblin
     }
 }
