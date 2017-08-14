6c0403759ba52c55fc5401403c65e8002661819a
BuildTools <fischer.alexander93@googlemail.com>
2017-08-14 12:23:09 +0200

    rework

--- GameServer/Character.cs
+++ GameServer/Character.cs
@@ -3,26 +3,8 @@ using GameData;
 
 namespace GameServer
 {
-    public class Character : Network.Data.Character
+    public class Character
     {
-        public static Character GetCharacterById(long Id)
-        {
-            Character character = new Character();
-            character.Id = Id;
-            character.Name = "Hellfire";
-            character.Level = 2;
-            character.Race = Race.Human;
-            character.Class = Class.Rouge;
-            character.Experience = 10000;
-            character.Location = new Location()
-            {
-                CoordX = 0f,
-                CoordY = 0f,
-                CoordZ = 0f,
-                Map = 0,
-                Name = "Blup"
-            };
-            return character;
-        }
+        SqlCharacter SqlCharacter { get; set; }
     }
 }
