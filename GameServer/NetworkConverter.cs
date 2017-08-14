362e5427ad7005582df6aad25304d073b9c7c171
BuildTools <fischer.alexander93@googlemail.com>
2017-08-14 12:24:40 +0200

    created
    maybe there is a better way to send data to user without converting it

---
+++ GameServer/NetworkConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace GameServer
+{
+    public static class NetworkConverter
+    {
+        public static Network.Data.Character NetworkCharacter(SqlCharacter character)
+        {
+            return new Network.Data.Character()
+            {
+                Id = character.Id,
+                Class = character.Class,
+                Experience = character.Exp,
+                Level = character.Level,
+                Location = character.Location,
+                Name = character.Name,
+                Race = character.Race,
+                Fraction = character.Fraction
+            };
+        }
+    }
+}
