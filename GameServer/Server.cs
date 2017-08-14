0ae513fec292a07ce9dc3d367eeddc95e67f23c0
BuildTools <fischer.alexander93@googlemail.com>
2017-08-14 12:29:37 +0200

    update
    + added Settings Class
    + created ServerUser Class and changed to account Dictionary to ServerUser. ServerUser contains an Account and additional information about the connection (LastActivity, IpEndPoint). Also disconnects user if no activity detected
    + Added Environment Class to Globaly use Server Managing functions
    + reworked some functions
    + Added Create Character function

--- GameServer/Server.cs
+++ GameServer/Server.cs
@@ -4,39 +4,99 @@ using Network.Packages;
 using System.Collections.Generic;
 using System.Net;
 using FileManager;
+using System.Threading;
 
 namespace GameServer
 {
-    static class Globals
+    public class ServerUser
     {
-        public static Config ConfigurationFile = new Config("config.ini");
+        public static double TimeoutTimeInMinutes = 1;
 
-        public static SqlBase SqlBase = new SqlBase();
+        public IPEndPoint IPEndPoint { get; private set; }
+        public Account Account { get; private set; }
+        public DateTime LastActivity { get; private set; }
+
+        public event Action OnTimeout;
+
+        private Thread TimeoutThread;
+
+
+        public ServerUser(Account account, IPEndPoint ipendpoint)
+        {
+            Account = account;
+            IPEndPoint = ipendpoint;
+            RefreshActivity();
+
+            TimeoutThread = new Thread(TimeoutThreadFunction);
+            TimeoutThread.Start();
+        }
+
+        public void RefreshActivity()
+        {
+            LastActivity = DateTime.Now;
+        }
+
+        private void TimeoutThreadFunction()
+        {
+            while (LastActivity.AddMinutes(TimeoutTimeInMinutes) > DateTime.Now)
+            {
+                // Check once a minute
+                Thread.Sleep(60000);
+            }
+
+            OnTimeout?.Invoke();
+        }
+    }
+
+    static class Settings
+    {
+        public const string configuration_file = "config.ini";
+        public const string database_file = "data.sqlite";
+
+        public const double timeout_in_minutes = 15d;
+    }
+
+    static class Global
+    {
+        public static Config ConfigurationFile;
+
+        public static SqlBase SqlBase;
 
         public static string Name = "Example Name";
         public static int Port = 5090;
         public static List<long> Admins = new List<long>();
     }
 
+    static class Environment
+    {
+        public static void Stop(string reason = "")
+        {
+            //todo: save all data
+            if (reason != "")
+            {
+                Logger.Error("Server stopped! Reason: " + reason);
+            }
+            Logger.Log("Press Enter to quit");
+            Console.ReadLine();
+            System.Environment.Exit(-1);
+        }
+    }
+
     class MainClass
     {
-        public static Dictionary<IPEndPoint, Account> _accounts = new Dictionary<IPEndPoint, Account>();
+        public static Dictionary<IPEndPoint, ServerUser> _dictServerUser = new Dictionary<IPEndPoint, ServerUser>();
         public static UdpServer _server;
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Starting Server ...");
-            LoadConfigurationFile();
-            ApplyStartupArgs(args);
-
-            SetupCommands();
-
-            Console.WriteLine("Listening on Port: " + Globals.Port);
-            _server = new UdpServer(Globals.Port);
-            _server.DataReceived += OnServerReceive;
-            _server.Start();
-            Console.WriteLine("Server started");
-            Console.WriteLine("Server Name: " + Globals.Name);
+            try
+            {
+                StartupServer(args);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.Message);
+            }
             while (true)
             {
                 string input = Console.ReadLine();
@@ -46,15 +106,49 @@ namespace GameServer
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("Error: " + ex.Message);
+                    Logger.Error(ex.Message);
                 }
             }
             // Environment.Exit(0);
         }
 
+        public static void StartupServer(string[] args)
+        {
+            DateTime startTime = DateTime.Now;
+            Logger.Log("Starting Server");
+
+            // Database
+            LoadDatabaseFile();
+
+            // Configuration File
+            LoadConfigurationFile();
+
+            ApplyStartupArgs(args);
+
+            SetupCommands();
+
+            Logger.Log("Listening on Port: " + Global.Port);
+            _server = new UdpServer(Global.Port);
+            _server.DataReceived += OnServerReceive;
+            _server.Start();
+            Logger.Log("Server started");
+            Logger.Log("Startup took " + DateTime.Now.Subtract(startTime).TotalSeconds + " seconds");
+            Logger.Log("Server Name: " + Global.Name);
+        }
+
+        public static void LoadDatabaseFile()
+        {
+            Global.SqlBase = new SqlBase();
+            Logger.Log("Loading Maps ... ", true);
+            MapManager.LoadMaps(Global.SqlBase);
+            Logger.Log("Done");
+
+
+        }
+
         public static void SetupCommands()
         {
-            Console.WriteLine("Setup commands");
+            Logger.Log("Setup commands ... ", true);
             CommandListener.AddCommand(CommandManager.Quit);
             CommandListener.AddCommand(CommandManager.ChangeName);
             CommandListener.AddCommand(CommandManager.Online);
@@ -62,32 +156,38 @@ namespace GameServer
             CommandListener.AddCommand(CommandManager.Help);
             CommandListener.AddCommand(CommandManager.Show);
             CommandListener.AddCommand(CommandManager.Account);
+            CommandListener.AddCommand(CommandManager.Character);
+            Logger.Log("Done");
         }
 
         public static void LoadConfigurationFile()
         {
-            Console.WriteLine("Loading Configuration");
-            foreach (Config.KeyValue keyvalue in Globals.ConfigurationFile.GetConfigs())
+            Logger.Log("Loading Configuration File ... ", true);
+
+            Global.ConfigurationFile = new Config(Settings.configuration_file);
+
+            foreach (Config.KeyValue keyvalue in Global.ConfigurationFile.GetConfigs())
             {
                 switch (keyvalue.Key.ToLower())
                 {
                     case "name":
-                        Globals.Name = keyvalue.Value;
+                        Global.Name = keyvalue.Value;
                         break;
                     case "port":
-                        int.TryParse(keyvalue.Value, out Globals.Port);
+                        int.TryParse(keyvalue.Value, out Global.Port);
                         break;
                     case "admins":
                         foreach (string id in keyvalue.Value.Split(','))
                         {
                             if (long.TryParse(id, out long Id))
                             {
-                                Globals.Admins.Add(Id);
+                                Global.Admins.Add(Id);
                             }
                         }
                         break;
                 }
             }
+            Logger.Log("Done");
         }
 
         public static void ApplyStartupArgs(string[] args)
@@ -96,34 +196,42 @@ namespace GameServer
             {
                 if (args[i] == "-p")
                 {
-                    int.TryParse(args[i + 1], out Globals.Port);
+                    int.TryParse(args[i + 1], out Global.Port);
                 }
             }
+            Account.TimeoutInMinutes = Settings.timeout_in_minutes;
         }
 
         public static void OnServerReceive(object sender, NetworkReceiveEventArgs e)
         {
-            if (!_accounts.ContainsKey(e.Sender))
+            if (!_dictServerUser.ContainsKey(e.Sender))
             {
-                _accounts.Add(e.Sender, new Account());
+                ServerUser serverUser = new ServerUser(new Account(), e.Sender);
+                serverUser.OnTimeout += () =>
+                {
+                    Logger.Log("[" + e.Sender.Address + "] was disconnected. Reason: Timeout");
+                    serverUser.Account.Logout();
+                    _dictServerUser.Remove(serverUser.IPEndPoint);
+                };
+                _dictServerUser.Add(e.Sender, serverUser);
             }
-            Account account = _accounts[e.Sender];
-
-            Console.WriteLine("Received something");
+            _dictServerUser[e.Sender].RefreshActivity();
+            Account account = _dictServerUser[e.Sender].Account;
 
             switch (NetworkHelper.GetPackageType(e.Data))
             {
                 #region "Login"
                 case PackageType.Login:
                     LoginRequest request = NetworkHelper.Deserialize<LoginRequest>(e.Data);
-                    if (account.Login(request.Username, request.Password))
+                    GameData.ErrorResult loginResult = account.Login(request.Username, request.Password);
+                    if (loginResult == GameData.ErrorResult.Success)
                     {
-                        _server.SendBytes(e.Sender, NetworkHelper.Serialize(new LoginResponse() { Success = true }));
-                        Console.WriteLine("Authentificated");
+                        _server.SendBytes(e.Sender, NetworkHelper.Serialize(new LoginResponse { Success = true }));
+                        Logger.Log("[" + e.Sender.Address + "] " + account.Username + " connected to Server");
                     }
                     else
                     {
-                        _server.SendBytes(e.Sender, NetworkHelper.Serialize(new LoginResponse()));
+                        _server.SendBytes(e.Sender, NetworkHelper.Serialize(new LoginResponse { Error = loginResult }));
                     }
                     break;
                 #endregion
@@ -144,7 +252,7 @@ namespace GameServer
                 #endregion
                 #region "GetAccountCharacters"
                 case PackageType.GetAccountCharacters:
-                    GetAccountCharactersResponse gacr = new GetAccountCharactersResponse() { Success = true, Characters = account.GetCharacters() };
+                    GetAccountCharactersResponse gacr = new GetAccountCharactersResponse() { Success = true, Characters = account.GetNetworkCharacters() };
                     _server.SendBytes(e.Sender, NetworkHelper.Serialize(gacr));
                     break;
                 #endregion
@@ -152,19 +260,61 @@ namespace GameServer
                 case PackageType.SelectCharacter:
                     SelectCharacterRequest selectCharacterRequest = NetworkHelper.Deserialize<SelectCharacterRequest>(e.Data);
                     SelectCharacterResponse scr = new SelectCharacterResponse();
-                    if (account.JoinWorld(Character.GetCharacterById(selectCharacterRequest.CharacterId)))
+                    if (account.JoinWorld(selectCharacterRequest.CharacterId))
                     {
                         scr.Success = true;
                     }
                     _server.SendBytes(e.Sender, NetworkHelper.Serialize(scr));
                     break;
                 #endregion
+
+                #region "ChreateCharacter"
+                case PackageType.CreateCharacter:
+                    if (!account.Authentificated)
+                    {
+                        account.Logout();
+                        _server.SendBytes(e.Sender, NetworkHelper.Serialize(new LogoutResponse { Success = true, Status = LogoutStatus.TitleScreen }));
+                        break;
+                    }
+                    CreateCharacterRequest createCharacterRequest = NetworkHelper.Deserialize<CreateCharacterRequest>(e.Data);
+                    Network.Data.Character characterData = createCharacterRequest.CharacterData;
+                    SqlCharacter character = SqlCharacter.Load(characterData.Name);
+                    if (character != null)
+                    {
+                        _server.SendBytes(e.Sender, NetworkHelper.Serialize(new CreateCharacterResponse()
+                        {
+                            Success = false,
+                            Error = GameData.ErrorResult.NameExists
+                        }));
+                        break;
+                    }
+                    GameData.ErrorResult creationResult = SqlCharacter.Create(
+                        _dictServerUser[e.Sender].Account, characterData.Name, characterData.Class,
+                        characterData.Race, 1, 0, new GameData.Location(MapManager.GetMap(1), 0f, 1f, 2f), characterData.Fraction);
+                    switch (creationResult)
+                    {
+                        case GameData.ErrorResult.Success:
+                            _server.SendBytes(e.Sender, NetworkHelper.Serialize(new CreateCharacterResponse()
+                            {
+                                Success = true,
+                                Error = GameData.ErrorResult.Success
+                            }));
+                            break;
+                        default:
+                            _server.SendBytes(e.Sender, NetworkHelper.Serialize(new CreateCharacterResponse()
+                            {
+                                Success = false,
+                                Error = creationResult
+                            }));
+                            break;
+                    }
+                    break;
+                #endregion
                 default:
-                    Console.WriteLine("Received Unknown Package");
+                    Logger.Error("Received Unknown Package");
                     break;
             }
         }
-
 
     }
 }
