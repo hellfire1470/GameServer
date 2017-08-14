0c3f84778c83455e0573cfacbc36252c2da376c6
BuildTools <fischer.alexander93@googlemail.com>
2017-08-14 12:22:57 +0200

    update
    + Created Interface for Account
    + Improved Login Function, Now with GameKey, Banned status, Hashed Passwords
    + small reworks

--- GameServer/Account.cs
+++ GameServer/Account.cs
@@ -1,47 +1,98 @@
 ﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using GameData;
 
 namespace GameServer
 {
-    class Account : Network.Data.Account
+
+    public interface IAccount
     {
+        int Id { get; }
+        string Username { get; }
+        string Password { get; }
+        int MaxCharacters { get; }
+        string GameKey { get; }
+        bool Banned { get; }
+    }
+
+
+    public class Account : IAccount
+    {
+        // This is overwritten in Server->ApplyStartupArgs function
+        public static double TimeoutInMinutes = 15;
+
         public bool Authentificated { get; private set; } = false;
         public bool InGame { get; private set; } = false;
-        public Character Character { get; private set; } = null;
+        public SqlCharacter Character { get; private set; } = null;
 
-        public bool Login(string user, string password)
+        private int _id;
+        private string _username;
+        private string _password;
+        private int _maxCharacters;
+        private string _gameKey;
+        private bool _banned;
+
+        public int Id { get { return _id; } private set { _id = value; } }
+        public string Username { get { return _username; } private set { _username = value; } }
+        public string Password { get { return _password; } private set { _password = value; } }
+        public int MaxCharacters { get { return _maxCharacters; } private set { _maxCharacters = value; } }
+        public string GameKey { get { return _gameKey; } private set { _gameKey = value; } }
+        public bool Banned { get { return _banned; } private set { _banned = value; } }
+
+        //  public SqlAccount SqlAccount { get; private set; } = null;
+
+        public ErrorResult Login(string user, string password)
         {
-            SqlAccountData sqlData = Globals.SqlBase.GetAccount(user);
+            IAccount iAccount = SqlAccount.Load(user);
             //todo:: hash password in database
-            if (sqlData != null && user == sqlData.Username && password == sqlData.Password)
+
+            if (iAccount.Username == user && iAccount.Password == SqlAccount.HashPassword(user, password))
             {
+
+                if (iAccount.GameKey == "")
+                    return ErrorResult.InvalidGameKey;
+
+                if (iAccount.Banned)
+                    return ErrorResult.Banned;
+
+                Id = iAccount.Id;
+                Username = iAccount.Username;
+                Password = iAccount.Password;
+                GameKey = iAccount.GameKey;
+                MaxCharacters = iAccount.MaxCharacters;
+                Banned = iAccount.Banned;
                 Authentificated = true;
-                Username = sqlData.Username;
-                Id = sqlData.Id;
+                return ErrorResult.Success;
             }
-            return Authentificated;
+            else
+                return ErrorResult.WrongData;
         }
 
         public void Logout()
         {
             // todo:: do background server tasks to safely logoff player
             LeaveWorld();
+            Id = 0;
+            Username = null;
+            Password = null;
             Authentificated = false;
         }
 
-        private bool CanJoinWorld(Character character)
+        private bool CanJoinWorld(int characterId)
         {
-            if (Authentificated && HasCharacter(character))
+            if (Authentificated && HasCharacter(characterId))
             {
                 return true;
             }
             return false;
         }
 
-        public bool JoinWorld(Character character)
+        public bool JoinWorld(int characterId)
         {
-            if (CanJoinWorld(character))
+            if (CanJoinWorld(characterId))
             {
-                Character = character;
+                Character = SqlCharacter.Load(characterId);
                 InGame = true;
                 return true;
             }
@@ -54,17 +105,27 @@ namespace GameServer
             Character = null;
         }
 
-        public Character[] GetCharacters()
+        public List<int> GetCharacterIdList()
         {
             if (Authentificated)
-                return new Character[] { Character.GetCharacterById(0) };
-            throw new Exception("Account not Authentificated");
+                return SqlAccount.GetCharacterIds(Id);
+            return new List<int>();
         }
 
-        private bool HasCharacter(Character character)
+        public Network.Data.Character[] GetNetworkCharacters()
         {
-            // todo: write function to check account has character
-            return true;
+            List<int> characterIds = GetCharacterIdList();
+            Network.Data.Character[] networkCharacters = new Network.Data.Character[characterIds.Count];
+            for (int i = 0; i < characterIds.Count; i++)
+            {
+                networkCharacters[i] = NetworkConverter.NetworkCharacter(SqlCharacter.Load(characterIds[i]));
+            }
+            return networkCharacters;
+        }
+
+        private bool HasCharacter(int characterId)
+        {
+            return GetCharacterIdList().Contains(characterId);
         }
     }
 }
