8e5909e948d875eebe381dea2fbc23c58175795e
BuildTools <fischer.alexander93@googlemail.com>
2017-08-14 12:24:07 +0200

    created
    All maps are stored here

---
+++ GameServer/Manager/MapManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Database;
+using GameData;
+
+public static class MapManager
+{
+    private static Dictionary<int, Map> _maps = new Dictionary<int, Map>();
+
+    public static Map GetMap(int id)
+    {
+        if (_maps.ContainsKey(id))
+            return _maps[id];
+        return null;
+    }
+
+    public static void AddMap(Map map){
+        _maps[map.Id] = map;
+    }
+
+    public static void LoadMaps(SQL sql){
+        Dictionary<int, Dictionary<string, string>> maps = sql.ExecuteQuery("select * from map");
+        foreach (Dictionary<string, string> map in maps.Values)
+        {
+            AddMap(new Map(){ Id = int.Parse(map["id"]), Name = map["name"]});
+        }
+    }
+}
